27ded1410ebb2eb251b9d622127c6b890b326334
BriHar <bdharrel@uno.edu>
2020-11-21 06:40:56 -0600

    Add files via upload

---
+++ assignment_7.py
@@ -0,0 +1,18 @@
+"""
+Bridget D. Harrell
+Assignment 7
+"""
+
+import my_module
+
+import pprint
+
+
+print(my_module.greeting("Bridget D. Harrell"))
+
+
+data = my_module.my_json_data
+
+# if error here do this "pip install prettyprint"
+
+pprint.pprint(data)
